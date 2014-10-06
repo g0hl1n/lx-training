e3d3677de95daa2e0024b98ecd85c6f4ec0a2553
Richard Leitner <me@g0hl1n.net>
2014-10-06 17:20:40 +0200

    lab02: add time information to hello_version module

--- labs/02_hello_version_module/hello_version.c
+++ labs/02_hello_version_module/hello_version.c
@@ -7,6 +7,7 @@
 #define DRIVER_DESC   "Hello-Version Test Module"
 
 static char *who = "nobody";
+static struct timeval starttime, endtime;
 
 module_param(who, charp, S_IRUGO);
 MODULE_PARM_DESC(who, "Your name");
@@ -15,7 +16,12 @@ static int __init hello_version_init(void)
 {
 	struct new_utsname *u;
 
+	/* get start time */
+	do_gettimeofday(&starttime);
+
+	/* get kernel version */
 	u = utsname();
+
 	printk(KERN_INFO "Hello %s, you are running Linux %s\n",
 	       who, u->release);
 	return 0;
@@ -23,7 +29,14 @@ static int __init hello_version_init(void)
 
 static void __exit hello_version_exit(void)
 {
-	printk(KERN_INFO "bye %s... :-(\n", who);
+	unsigned long run_ms;
+
+	/* get end time & calc running time */
+	do_gettimeofday(&endtime);
+	run_ms = (endtime.tv_sec - starttime.tv_sec) * 1000 +
+		 (endtime.tv_usec - starttime.tv_usec) / 1000;
+
+	printk(KERN_INFO "Bye %s, the %lums with you were nice!\n", who, run_ms);
 }
 
 module_init(hello_version_init);
